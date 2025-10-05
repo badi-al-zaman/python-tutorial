c1d70e318b975f97165bee66fdb1f60293823aa4
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-10-05 14:53:08 -0700

    data types of variables

---
+++ beginner/02_variables.py
@@ -0,0 +1,13 @@
+x1 = str("Hello, World!")
+x2 = int(1)
+x3 = float(1.1)
+x4 = complex(10, 5)
+x5 = bool(True)
+x6 = list((1, 2, 3))
+x7 = tuple((1, 2, 3))
+x9 = dict(name="John", age=36)
+x8 = range(6)
+print(type(x1))
+
+print("10"+"10") # string concatenation results is: "1010"
+print(10+10) # integer addition results is: 20
