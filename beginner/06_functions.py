857070873fe2a6fa904039ce44f1a7efa35f17f7
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-10-05 16:16:46 -0700

    review functions

--- beginner/06_functions.py
+++ beginner/06_functions.py
@@ -1,7 +1,10 @@
 def mysum(*numbers): # * Mean Passing many arguments
-	count = 0
-	for num in numbers:
-		count += num
-	return count
+    count = 0
+    for num in numbers:
+        count += num
+    return count
 
 print(mysum(1,2,3,4,5)) # 15
+
+def person(name="John", age=30): # Default values
+    print(f"name: {name}, age: {age}")
