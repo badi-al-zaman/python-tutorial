b994b4139cf1f0dc86184d58e8b9eba5d1335b03
badi-al-zaman <badi.al.zaman2446@gmail.com>
2025-10-05 16:28:43 -0700

    review functions

---
+++ beginner/07_exception_handling.py
@@ -0,0 +1,30 @@
+def divide():
+    try:
+        x = int(input("First Number: "))
+        y = int(input("Second Number: "))
+        print(x / y)
+    except ValueError:
+        print("Please Enter A Valid Number.")
+        print("starting over...")
+        divide()
+    except ZeroDivisionError:
+        print("Can't Divide By Zero")
+        print("starting over...")
+        divide()
+    finally:
+        print("Done")
+
+divide()
+
+def test():
+    if True:
+        raise Exception("Test")
+        # raise ValueError("Test")
+
+# test()
+
+x = 100
+y = 20
+
+# Return Error & Finish The Script If The Assertion Is Not True
+# assert (x < y)
